6ff10ccb28afaec5f711172b8a198be6bbd57bb6
Chris Llanwarne <chrisl@broadinstitute.org>
2015-09-10 15:03:26 -0400

    Fixed the liquibase script to allow deployment.

--- src/main/resources/liquibase/liquibase-deploy.sh
+++ src/main/resources/liquibase/liquibase-deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 liquibase --driver=com.mysql.jdbc.Driver \
-     --classpath
-     --changeLogFile=src/main/resources/liquibase/changelog.xml \
+     --classpath=/Users/chrisl/Downloads/mysql-connector-java-5.1.36/mysql-connector-java-5.1.36-bin.jar \
+     --changeLogFile=/Users/chrisl/IdeaProjects/thurloe/src/main/resources/liquibase/changelog.xml \
      --url="jdbc:mysql://localhost/example" \
      --username=root \
      --password="" \
      migrate
-
-#   Is this line needed??
-#      --classpath=/path/to/classes \
