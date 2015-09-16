1bad9826bd9df86be38f3f83e8f945e4b1b0a754
Chris Llanwarne <chrisl@broadinstitute.org>
2015-09-16 11:07:44 -0400

    Moved liquibase to new folder.

--- src/main/migrations/liquibase-deploy.sh
+++ src/main/migrations/liquibase-deploy.sh
@@ -5,7 +5,7 @@
 
 liquibase --driver=com.mysql.jdbc.Driver \
     --classpath=${HOME}/.ivy2/cache/mysql/mysql-connector-java/jars/mysql-connector-java-5.1.35.jar \
-    --changeLogFile=src/main/resources/liquibase/changelog.xml \
+    --changeLogFile=src/main/migrations/changelog.xml \
     --url="jdbc:mysql://localhost/thurloe_test" \
     --username=travis \
     --password="" \
