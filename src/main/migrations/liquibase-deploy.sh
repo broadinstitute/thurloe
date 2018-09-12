a4d338af71fbd4f326b8ecc1c1e0b222e0b51e91
dvoet <dvoet@users.noreply.github.com>
2018-09-11 22:37:32 -0400

    automatically run liquibase on boot (#64)

--- src/main/migrations/liquibase-deploy.sh
+++ src/main/migrations/liquibase-deploy.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-#
-# Can be used for initial setup or incremental updates of a local Thurloe backing database.
-#
-
-liquibase --driver=com.mysql.jdbc.Driver \
-    --classpath=${HOME}/.ivy2/cache/mysql/mysql-connector-java/jars/mysql-connector-java-5.1.35.jar \
-    --changeLogFile=src/main/migrations/changelog.xml \
-    --url="jdbc:mysql://localhost/thurloe_test" \
-    --username="travis" \
-    --password="" \
-    migrate
+# no longer required
