276b1a3925e0da288897abc8650d27114e55f2d8
Isaac Zarsky <zarsky@broadinstitute.org>
2020-12-18 12:23:19 -0500

    Add trivy scanning of docker image [AS-548] (#74)

--- docker/build.sh
+++ docker/build.sh
@@ -98,6 +98,9 @@ function docker_cmd()
 
         echo "building ${DOCKERHUB_REGISTRY}:${HASH_TAG}..."
         docker build -t $DOCKERHUB_REGISTRY:${HASH_TAG} .
+	
+        echo "scanning docker image..."
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/trivy-cache:/root/.cache/ aquasec/trivy --severity CRITICAL --no-progress --ignore-unfixed --exit-code 1 "$DOCKERHUB_REGISTRY":"${HASH_TAG}"
 
         if [ $DOCKER_CMD="push" ]; then
             echo "pushing ${DOCKERHUB_REGISTRY}:${HASH_TAG}..."
