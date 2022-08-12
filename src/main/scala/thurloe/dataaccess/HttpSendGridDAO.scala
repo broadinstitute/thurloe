96cb1cca253a3af04c972bbd93c9ed12a33da4a9
Matt Bemis <mbemis@broadinstitute.org>
2022-08-12 17:49:30 -0400

    proposed fix

--- src/main/scala/thurloe/dataaccess/HttpSendGridDAO.scala
+++ src/main/scala/thurloe/dataaccess/HttpSendGridDAO.scala
@@ -5,7 +5,7 @@ import com.sendgrid.SendGrid.Response
 import com.sendgrid._
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.model.{WorkbenchEmail, WorkbenchUserId}
-import thurloe.database.ThurloeDatabaseConnector
+import thurloe.database.{KeyNotFoundException, ThurloeDatabaseConnector}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -32,18 +32,35 @@ class HttpSendGridDAO extends SendGridDAO with LazyLogging {
     }
   }
 
-  def lookupPreferredEmail(userId: WorkbenchUserId): Future[WorkbenchEmail] =
-    for {
-      email <- dataAccess
-        .lookup(userId.value, "contactEmail")
-        .recoverWith { _ =>
-          val fallbackEmail = dataAccess.lookup(userId.value, "email")
-          logger.info(
-            s"Failed to get stored contactEmail for ${userId.value}. Defaulting to stored email ($fallbackEmail) for user."
-          )
-          fallbackEmail
+  //Looks up a KVP, converting empty values or missing KVPs into None
+  private def lookupNonEmptyKeyValuePair(userId: String, key: String) = {
+    dataAccess.lookup(userId, key).map { kvp =>
+      if(kvp.keyValuePair.value.isEmpty) None
+      else Some(kvp)
+    }.recover { _ => None }
+  }
+
+  //There are two cases that need to be handled when looking up the preferred contact email:
+  // 1) If the contactEmail is not present at all, the DB query will throw an exception. So that needs to be handled.
+  // 2) If the contactEmail is present but blank, it also needs to be ignored. Thurloe accepts arbitrary key/value pairs
+  //    and makes no guarantees about what the data might look like, so a blank value or invalid email is a valid case.
+  //    Orchestration does it's best to enforce valid emails, but blank emails are still used when a user removes their
+  //    contact email (instead of deleting the k/v pair entirely). There is a larger discussion to be had about the future
+  //    of this service and how to structure the data more reliably, but for now we'll deal with the current realities and
+  //    defend against them here.
+  //  ^-- note: the same can also all be said about the 'email' k/v pair, but a blank email is a less common case due to
+  //      how profiles are populated.
+  def lookupPreferredEmail(userId: WorkbenchUserId): Future[WorkbenchEmail] = {
+    lookupNonEmptyKeyValuePair(userId.value, "contactEmail") flatMap {
+      case Some(kvp) => Future.successful(WorkbenchEmail(kvp.keyValuePair.value)) //contactEmail was found and non-empty
+      case None =>
+        logger.info(s"Failed to get stored contactEmail for ${userId.value}. Defaulting to account email for user.")
+        lookupNonEmptyKeyValuePair(userId.value, "email") flatMap {
+          case Some(kvp) => Future.successful(WorkbenchEmail(kvp.keyValuePair.value)) //account email was found and non-empty
+          case None => Future.failed(new KeyNotFoundException(userId.value, "email"))
         }
-    } yield WorkbenchEmail(email.keyValuePair.value)
+    }
+  }
 
   def lookupUserName(userId: WorkbenchUserId): Future[String] =
     for {
