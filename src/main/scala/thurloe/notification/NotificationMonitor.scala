07e94821829bc681584a6d7ff4b4a513b8a7b9b9
Matt Bemis <MatthewBemis@users.noreply.github.com>
2022-10-05 15:01:17 -0400

    [SUP-519] Add killswitch for workspace-level notifications (#86)
    * add killswitch for workspace notifs

--- src/main/scala/thurloe/notification/NotificationMonitor.scala
+++ src/main/scala/thurloe/notification/NotificationMonitor.scala
@@ -194,12 +194,28 @@ class NotificationMonitorActor(val pollInterval: FiniteDuration,
 
   def lookupShouldNotify(notification: Notification): Future[Boolean] =
     notification match {
+      //For workspace notifications, there are three tiers of preferences to check:
+      // 1. has the user disabled *all* notifications for their account?
+      //     key format: notifications.off
+      // 2. has the user disabled the notification at the type-level?
+      //     key format: notifications/<notification class name>
+      // 3. has the user disabled the notification for the specified workspace?
+      //     key format: notifications/<notification class name>/<workspace namespace>/<workspace name>
+      case wsNotification: WorkspaceNotification =>
+        val baseKey = s"notifications/${wsNotification.getClass.getSimpleName}"
+        booleanLookup(wsNotification.recipientUserId, NotificationMonitor.notificationsOffKey, false) flatMap {
+          case false =>
+            booleanLookup(wsNotification.recipientUserId, baseKey, true) flatMap {
+              case false => Future.successful(false)
+              case true  => booleanLookup(wsNotification.recipientUserId, notification.key, true)
+            }
+          case true => Future.successful(false) // notifications off for this user
+        }
       case UserNotification(recipientId) =>
         booleanLookup(recipientId, NotificationMonitor.notificationsOffKey, false) flatMap {
           case false => booleanLookup(recipientId, notification.key, true)
           case true  => Future.successful(false) // notifications off for this user
         }
-
       case _ => Future.successful(true)
     }
 
