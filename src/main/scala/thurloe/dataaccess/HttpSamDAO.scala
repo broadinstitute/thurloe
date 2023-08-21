c7c6d053cf5107d81fba696a22eaf2638c6d759f
Tristan Garwood <tristan.garwood@gmail.com>
2023-08-21 13:29:45 -0400

    Add credential refresh

--- src/main/scala/thurloe/dataaccess/HttpSamDAO.scala
+++ src/main/scala/thurloe/dataaccess/HttpSamDAO.scala
@@ -1,6 +1,7 @@
 package thurloe.dataaccess
 
 import akka.actor.ActorSystem
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import okhttp3.Protocol
@@ -21,27 +22,35 @@ class HttpSamDAO(config: Config, credentials: GoogleCredentialModes.Pem)(implici
   private val samServiceURL = samConfig.getString("samBaseUrl")
   private val timeout = samConfig.getDuration("timeout").toScala
 
-  private val okHttpClient = new ApiClient().getHttpClient
+  private def getApiClient = {
+    val okHttpClient = new ApiClient().getHttpClient
 
-  val okHttpClientBuilder = okHttpClient.newBuilder
-    .readTimeout(timeout.toJava)
+    val okHttpClientBuilder = okHttpClient.newBuilder
+      .readTimeout(timeout.toJava)
 
-  val samApiClient = new ApiClient(okHttpClientBuilder.protocols(Seq(Protocol.HTTP_1_1).asJava).build())
-  samApiClient.setBasePath(samServiceURL)
+    val samApiClient = new ApiClient(okHttpClientBuilder.protocols(Seq(Protocol.HTTP_1_1).asJava).build())
+    samApiClient.setBasePath(samServiceURL)
 
-  //Set credentials
-  val scopes = List.empty
-  val token = credentials.toGoogleCredential(scopes).getAccessToken
-  samApiClient.setAccessToken(credentials.toGoogleCredential(scopes).getAccessToken)
-
-  protected def adminApi() = new AdminApi(samApiClient)
-
-  system.log.info(s"Using access token: $token")
-
-  override def getUserById(userId: String): List[sam.model.User] = {
-    system.log.info(s"Using access token: $token")
-    system.log.info(samApiClient.getAuthentications.asScala.mkString("\n"))
-    adminApi().adminGetUsersByQuery(userId, userId, userId, 5).asScala.toList
+    //Set credentials
+    val scopes = List.empty
+    val saPemCredentials: GoogleCredential = credentials.toGoogleCredential(scopes)
+    val token = getServiceAccountAccessToken(saPemCredentials)
+    system.log.info(s"Using credentials for sam: $token")
+    samApiClient.setAccessToken(getServiceAccountAccessToken(saPemCredentials))
+    samApiClient
   }
+
+  private def getServiceAccountAccessToken(saPemCredentials: GoogleCredential): String = {
+    val expiresInSeconds = Option(saPemCredentials.getExpiresInSeconds).map(_.longValue()).getOrElse(0L)
+    if (expiresInSeconds < 60 * 5) {
+      saPemCredentials.refreshToken()
+    }
+    saPemCredentials.getAccessToken
+  }
+
+  protected def adminApi(samApiClient: ApiClient) = new AdminApi(samApiClient)
+
+  override def getUserById(userId: String): List[sam.model.User] =
+    adminApi(getApiClient).adminGetUsersByQuery(userId, userId, userId, 5).asScala.toList
 
 }
