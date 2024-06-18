a59c2a28a04c3f24cd6ea979e0b3b0a8053914b5
giventocode <3589801+giventocode@users.noreply.github.com>
2024-06-18 15:43:15 -0400

    disable notification monitor supervisor

--- src/main/scala/thurloe/Main.scala
+++ src/main/scala/thurloe/Main.scala
@@ -4,17 +4,11 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import com.typesafe.config.ConfigFactory
 import io.sentry.{Sentry, SentryOptions}
-import org.broadinstitute.dsde.workbench.google.{GoogleCredentialModes, HttpGooglePubSubDAO}
-import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
-import org.broadinstitute.dsde.workbench.util.toScalaDuration
+import thurloe.dataaccess.HttpSamDAO
 import thurloe.dataaccess.auth.CloudServiceAuthTokenProvider
-import thurloe.dataaccess.{HttpSamDAO, HttpSendGridDAO}
-import thurloe.notification.NotificationMonitorSupervisor
 import thurloe.service.ThurloeServiceActor
 
-import java.io.File
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.jdk.CollectionConverters._
 
 object Main extends App {
   val sentryDsn = sys.env.get("SENTRY_DSN")
@@ -35,36 +29,36 @@ object Main extends App {
 
   val samDao = new HttpSamDAO(config, cloudAuthProvider)
 
-  val pem =
-    GoogleCredentialModes.Pem(WorkbenchEmail(gcsConfig.getString("clientEmail")),
-                              new File(gcsConfig.getString("pathToPem")))
-  val pubSubDAO = new HttpGooglePubSubDAO(
-    gcsConfig.getString("appName"),
-    pem,
-    "thurloe",
-    gcsConfig.getString("serviceProject")
-  )
+//  val pem =
+//    GoogleCredentialModes.Pem(WorkbenchEmail(gcsConfig.getString("clientEmail")),
+//                              new File(gcsConfig.getString("pathToPem")))
+//  val pubSubDAO = new HttpGooglePubSubDAO(
+//    gcsConfig.getString("appName"),
+//    pem,
+//    "thurloe",
+//    gcsConfig.getString("serviceProject")
+//  )
 
-  private val httpSendGridDAO = new HttpSendGridDAO(samDao)
-  system.actorOf(
-    NotificationMonitorSupervisor.props(
-      toScalaDuration(gcsConfig.getDuration("notificationMonitor.pollInterval")),
-      toScalaDuration(gcsConfig.getDuration("notificationMonitor.pollIntervalJitter")),
-      pubSubDAO,
-      gcsConfig.getString("notificationMonitor.topicName"),
-      gcsConfig.getString("notificationMonitor.subscriptionName"),
-      gcsConfig.getInt("notificationMonitor.workerCount"),
-      httpSendGridDAO,
-      config
-        .getConfig("notification.templateIds")
-        .entrySet()
-        .asScala
-        .map(entry => entry.getKey -> entry.getValue.unwrapped().toString)
-        .toMap,
-      config.getString("notification.fireCloudPortalUrl"),
-      samDao
-    )
-  )
+//  private val httpSendGridDAO = new HttpSendGridDAO(samDao)
+//  system.actorOf(
+//    NotificationMonitorSupervisor.props(
+//      toScalaDuration(gcsConfig.getDuration("notificationMonitor.pollInterval")),
+//      toScalaDuration(gcsConfig.getDuration("notificationMonitor.pollIntervalJitter")),
+//      pubSubDAO,
+//      gcsConfig.getString("notificationMonitor.topicName"),
+//      gcsConfig.getString("notificationMonitor.subscriptionName"),
+//      gcsConfig.getInt("notificationMonitor.workerCount"),
+//      httpSendGridDAO,
+//      config
+//        .getConfig("notification.templateIds")
+//        .entrySet()
+//        .asScala
+//        .map(entry => entry.getKey -> entry.getValue.unwrapped().toString)
+//        .toMap,
+//      config.getString("notification.fireCloudPortalUrl"),
+//      samDao
+//    )
+//  )
 
   val routes = new ThurloeServiceActor(samDao)
 
