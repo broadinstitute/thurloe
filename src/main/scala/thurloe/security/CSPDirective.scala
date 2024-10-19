0ce1f8a5dab92fc33be99a5c5f88adb32c28cd4f
Kevin Marete <kevinmmarete@gmail.com>
2024-10-18 22:13:50 -0400

    Scalafmt

--- src/main/scala/thurloe/security/CSPDirective.scala
+++ src/main/scala/thurloe/security/CSPDirective.scala
@@ -5,7 +5,10 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
 object CSPDirective {
-  private val cspHeader = RawHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; connect-src 'self'; form-action 'none';")
+  private val cspHeader = RawHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; connect-src 'self'; form-action 'none';"
+  )
 
   def addCSP(route: Route): Route = respondWithHeader(cspHeader) {
     route
