8cec42edc2b41118aa3bc47ab83eb492fcbcfea9
Kevin Marete <kevinmmarete@gmail.com>
2024-10-18 22:42:20 -0400

    Formatting test file

--- src/test/scala/thurloe/service/ThurloeServiceActorSpec.scala
+++ src/test/scala/thurloe/service/ThurloeServiceActorSpec.scala
@@ -1,23 +1,20 @@
 package thurloe.service
 
-import org.mockito.MockitoSugar.mock
-import thurloe.dataaccess.{HttpSamDAO}
-import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.matchers.should.Matchers
+import org.mockito.MockitoSugar.mock
 import org.scalatest.funspec.AnyFunSpec
-import thurloe.service.ThurloeServiceActor
+import org.scalatest.matchers.should.Matchers
+import thurloe.dataaccess.HttpSamDAO
 
 class ThurloeServiceActorSpec extends AnyFunSpec with Matchers with ScalatestRouteTest {
 
   val service = new ThurloeServiceActor(mock[HttpSamDAO])
 
-  describe("ThurloeServiceActor"){
-
-    it("include Content-Security-Policy header in main route"){
+  describe("ThurloeServiceActor") {
+    it("include Content-Security-Policy header in main route") {
       Get("/") ~> service.route ~> check {
-        val cspHeader: Option[HttpHeader] = header("Content-Security-Policy")
+        val cspHeader = header("Content-Security-Policy")
         cspHeader shouldBe defined
         cspHeader.get.value should include("default-src 'self'")
         cspHeader.get.value should include("script-src 'self' 'unsafe-inline'")
