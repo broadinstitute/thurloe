50979b82fd235bcc5c183feb5bd9ad9144662379
Kevin Marete <kevinmmarete@gmail.com>
2024-10-18 21:59:13 -0400

    Added CSP directive

---
+++ src/main/scala/thurloe/security/CSPDirective.scala
@@ -0,0 +1,13 @@
+package thurloe.security
+
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
+object CSPDirective {
+  private val cspHeader = RawHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; connect-src 'self'; form-action 'none';")
+
+  def addCSP(route: Route): Route = respondWithHeader(cspHeader) {
+    route
+  }
+}
