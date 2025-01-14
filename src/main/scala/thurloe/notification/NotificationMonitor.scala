2a6339994eec48c9d9113968a16c490bd8a8cf04
David An <davidan@genomebridge.org>
2025-01-14 16:05:34 -0500

    Revert "Revert "CORE-235: sendgrid-java 4.10.3 and corresponding syntax chang…"
    This reverts commit 937dfd121e916ebb553cb24e0dd8c4cdb3960cec.

--- src/main/scala/thurloe/notification/NotificationMonitor.scala
+++ src/main/scala/thurloe/notification/NotificationMonitor.scala
@@ -3,7 +3,7 @@ package thurloe.notification
 import akka.actor.SupervisorStrategy.{Escalate, Stop}
 import akka.actor._
 import akka.pattern._
-import com.sendgrid.SendGrid.Response
+import com.sendgrid.Response
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.google.GooglePubSubDAO
@@ -181,9 +181,9 @@ class NotificationMonitorActor(val pollInterval: FiniteDuration,
         .acknowledgeMessagesById(pubSubSubscriptionName, Seq(message.ackId))
         .map(_ => StartMonitorPass) pipeTo self
       responseOption match {
-        case Some(response: Response) if !response.getStatus =>
+        case Some(response: Response) if !sendGridDAO.isSuccessful(response) =>
           logger.error(
-            s"could not send notification ${message.contents}, sendgrid code: ${response.getCode}, sendgrid message: ${response.getMessage}"
+            s"could not send notification ${message.contents}, sendgrid code: ${response.getStatusCode}, sendgrid message: ${response.getBody}"
           )
         case _ => // log nothing
       }
