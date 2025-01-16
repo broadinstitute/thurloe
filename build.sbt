3d534c2dceaf5d51bc046d91a88f7988809f97cb
David An <davidan@broadinstitute.org>
2025-01-16 15:55:49 -0500

    support GroupAccessRequestNotificationV2 notifications

--- build.sbt
+++ build.sbt
@@ -14,9 +14,9 @@ val slickV = "3.5.2"
 
 val scalaTestV = "3.2.19"
 
-val workbenchLibsHash = "5d9c477"
-val workbenchGoogleV = s"0.33-$workbenchLibsHash"
-val workbenchNotificationsV = s"0.9-$workbenchLibsHash"
+val workbenchLibsHash = "4292239"
+val workbenchGoogleV = s"0.34-$workbenchLibsHash"
+val workbenchNotificationsV = s"0.10-$workbenchLibsHash"
 
 resolvers ++= Seq(
   "Broad Artifactory Releases" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release/",
--- src/main/scala/thurloe/notification/NotificationMonitor.scala
+++ src/main/scala/thurloe/notification/NotificationMonitor.scala
@@ -16,6 +16,7 @@ import thurloe.database.{DataAccess, KeyNotFoundException, ThurloeDatabaseConnec
 import thurloe.notification.NotificationMonitor.StartMonitorPass
 import thurloe.notification.NotificationMonitorSupervisor._
 
+import scala.annotation.nowarn
 import scala.concurrent.duration.{FiniteDuration, _}
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -257,6 +258,7 @@ class NotificationMonitorActor(val pollInterval: FiniteDuration,
   def groupManagementUrl(groupName: String): String = s"$fireCloudPortalUrl/#groups/${groupName}"
   def bucketUrl(bucketName: String): String = s"https://console.cloud.google.com/storage/browser/${bucketName}"
 
+  @nowarn("cat=deprecation")
   def toThurloeNotification(notification: Notification): thurloe.service.Notification = {
     val templateId = templateIdsByType(notification.getClass.getSimpleName)
 
@@ -425,11 +427,25 @@ class NotificationMonitorActor(val pollInterval: FiniteDuration,
                                      Map.empty
         )
 
+      // GroupAccessRequestNotification is deprecated. Once Sam switches over to sending
+      // GroupAccessRequestNotificationV2, delete this case and the @nowarn annotation
+      // on the enclosing method.
       case GroupAccessRequestNotification(recipientUserId, groupName, _, requesterId) =>
         thurloe.service.Notification(
           Option(recipientUserId),
           None,
           Option(requesterId),
+          templateId,
+          Map("groupName" -> groupName, "groupUrl" -> groupManagementUrl(groupName)),
+          Map("originEmail" -> requesterId),
+          Map("userNameFL" -> requesterId)
+        )
+
+      case GroupAccessRequestNotificationV2(recipientUserId, groupName, replyTo, requesterId) =>
+        thurloe.service.Notification(
+          Option(recipientUserId),
+          None,
+          Option(replyTo),
           templateId,
           Map("groupName" -> groupName, "groupUrl" -> groupManagementUrl(groupName)),
           Map("originEmail" -> requesterId),
